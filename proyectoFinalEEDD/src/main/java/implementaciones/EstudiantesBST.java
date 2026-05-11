764406a7e9681825288c97c0fca92855611c61ac
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-11 03:01:01 -0700

    Create EstudiantesBST.java

---
+++ proyectoFinalEEDD/src/main/java/implementaciones/EstudiantesBST.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementaciones;
+
+import objetos.Estudiante;
+
+/**
+ *
+ * @author joser
+ */
+public class EstudiantesBST {
+    BinarySearchTree<Estudiante> estudiantes;
+
+    public EstudiantesBST() {
+        this.estudiantes = new BinarySearchTree<>();
+    }
+    
+    /**
+     * Regresa el estudiante que encuentre con la misma matricula.
+     * @param matricula
+     * @return 
+     */
+    public Estudiante busquedaPorMatricula(String matricula){
+        return null;
+    }
+    
+}
