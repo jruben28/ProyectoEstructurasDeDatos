778e75d9391583ab973938c4fea752fd82502c86
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-11 03:01:08 -0700

    Create PromedioEstudiante.java

---
+++ proyectoFinalEEDD/src/main/java/implementaciones/PromedioEstudiante.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementaciones;
+
+import objetos.ParPromedioEstudiante;
+
+/**
+ *
+ * @author joser
+ */
+public class PromedioEstudiante {
+    AVLTree<ParPromedioEstudiante> parPromedioEstudiante;
+
+    public PromedioEstudiante() {
+        this.parPromedioEstudiante = new AVLTree<>();
+    }
+
+    /**
+     * Agrega al arbol AVL el par promedio de estudiante.
+     * @param estudiante 
+     */
+    public void agregarParPromedioEstudiante(ParPromedioEstudiante estudiante) {
+
+    }
+
+    /**
+     * Imprime el listado del promedio de estudiantes en orden ascendente.
+     */
+    public void imprimirListado(){
+    
+    }
+}
