3f9207f94660f76e9a2216293fb20adb6eebc315
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-11 03:01:06 -0700

    Create ParPromedioEstudiante.java

---
+++ proyectoFinalEEDD/src/main/java/objetos/ParPromedioEstudiante.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos;
+
+/**
+ *
+ * @author joser
+ */
+public class ParPromedioEstudiante implements Comparable<ParPromedioEstudiante>{
+    Estudiante estudiante;
+    float promedio;
+
+    public ParPromedioEstudiante(Estudiante estudiante, float promedio) {
+        this.estudiante = estudiante;
+        this.promedio = promedio;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    
+    
+    @Override
+    public int compareTo(ParPromedioEstudiante otro) {
+        return this.estudiante.getMatricula().compareTo(otro.estudiante.getMatricula());
+    }
+    
+
+}
