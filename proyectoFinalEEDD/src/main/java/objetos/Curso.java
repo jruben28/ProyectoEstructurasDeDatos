f3ac13f5df6244ad900dc7a14b72a87341479e23
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-11 02:34:22 -0700

    Esqueleto objeto curso

---
+++ proyectoFinalEEDD/src/main/java/objetos/Curso.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos;
+
+import implementaciones.LinkedList;
+
+/**
+ *
+ * @author joser
+ */
+public class Curso {
+    float clave;
+    String nombre;
+    LinkedList<Estudiante> listadoEstudiantes;
+    //CircularDoubleLinkedList<Estudiante> listaEspera;
+    //CircularLinkedList<Estudiante> roles;
+    Estudiante tutorActual;
+
+    public Curso(float clave, String nombre) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.listadoEstudiantes = new LinkedList<>();
+        //this.listaEspera = new CircularDoubleLinkedList<>();
+        //this.roles = new CircularLinkedList<>();
+    }
+    
+    /**
+     * Recorre la lista de espera adelante y regresa el siguiente estudiante
+     * @return 
+     */
+    public Estudiante recorrerAdelanteListaEspera(){
+        return null;
+    }
+    
+    /**
+     * Recorre la lista de espera atras y regresa el anterior estudiante
+     * @return 
+     */
+    public Estudiante recorrerAtrasListaEspera(){
+        return null;
+    }
+    
+    /**
+     * Regresa una lista de los primeros n estudiantes
+     * @param n
+     * @return 
+     */
+    public LinkedList<Estudiante> mostrarNPrimeros(int n){
+        return null;
+    }
+    
+    /**
+     * Regresa la lista completa de estudiantes en el curso
+     * @return 
+     */
+    public LinkedList<Estudiante> mostrarEstudiantes(){
+        return null;
+    }
+    
+    //public CircularDoubleLinkedList<Estudiante> mostrarListaEspera()
+    /**
+     * Actualiza la lista de los roles según los estudiantes actuales del curso.
+     */
+    public void actualizarListaRoles(){
+    
+    }
+    
+}
