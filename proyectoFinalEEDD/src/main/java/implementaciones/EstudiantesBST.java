2deacf9a2a05db8246f7f3dd569c62d58a201aca
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-17 01:54:09 -0700

    metodo depuracion

--- proyectoFinalEEDD/src/main/java/implementaciones/EstudiantesBST.java
+++ proyectoFinalEEDD/src/main/java/implementaciones/EstudiantesBST.java
@@ -41,4 +41,9 @@ public class EstudiantesBST {
         return null;
     }
     
+    
+    public void imprimirPrimero(){
+        System.out.println(estudiantes.firstEntry().toString());
+    }
+    
 }
